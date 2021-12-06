c532957e01b09ec782fcabe9982d4cfac6715881
Sergey-Zaitsev <73549064+Sergey-Zaitsev@users.noreply.github.com>
2021-12-06 23:07:23 +0300

    Добавлено hw3

--- GBShopTests/GBShopTests.swift
+++ GBShopTests/GBShopTests.swift
@@ -6,18 +6,58 @@
 //
 
 import XCTest
+import Alamofire
 @testable import GBShop
 
-class GBShopTests: XCTestCase {
+struct PostStub: Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
 
+enum ApiErrorStub: Error {
+    case fatalError
+}
+
+struct ErrorParserStub: AbstractErrorParser {
+    func parse(_ result: Error) -> Error {
+        return ApiErrorStub.fatalError
+    }
+    
+    func parse(response: HTTPURLResponse?, data: Data?, error: Error?) -> Error? {
+        return error
+    }
+}
+
+
+class GBShopTests: XCTestCase {
+    let expectation = XCTestExpectation(description: "Download https://failUrl")
+    var errorParser: ErrorParserStub!
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        errorParser = ErrorParserStub()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        errorParser = nil
     }
-
+    
+    func testShouldDownloadAndParse() {
+        AF
+            .request("https://jsonplaceholder.typicode.com/posts/1")
+            .responseCodable(errorParser: errorParser) { [weak self] (response: AFDataResponse<PostStub>) in
+                switch response.result {
+                case .success(_): break
+                case .failure:
+                    XCTFail()
+                }
+                self?.expectation.fulfill()
+            }
+        wait(for: [expectation], timeout: 10.0)
+        }
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -29,5 +69,6 @@ class GBShopTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
+
+
